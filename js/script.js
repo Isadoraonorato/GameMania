0fb1e86228179950a881a5fe45c3295c328fcd43
Isadoraonorato <isa.onorato@gmail.com>
2022-06-18 18:31:40 -0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
+$(document).ready(function(){
+
+$("#barras").click(function(){
+    $("#menu").toggleClas ("menu-ativo")
+
+
+
+//if($("#menu").hasClass ("menu-ativo")){
+//    $("#menu").removeClass ("menu-ativo")
+//}
+
+//else{
+//    $("#menu").addClass ("menu-ativo")
+//}
+
+
+})
+
+})
+
+
+
+
+
+
 let menu = document.getElementById ("menu");
 
-  function mostrarMenu (){
-      if (menu.style.display != "flex"){
-          menu.style.display = "flex"
-      }
-      else{
-          menu.style.display = "none"
-      }
-  }
+ // function mostrarMenu (){
+//   if (menu.style.display != "flex"){
+//          menu.style.display = "flex"
+//      }
+//      else{
+//          menu.style.display = "none"
+//      }
+//  }
 
  function mostrarPopup () {
     window.alert ("Parabens!")
@@ -19,3 +44,30 @@ function enviarEmail(){
     let emaillDigitado = email.valeu;
     console.log(emaillDigitado)
 }
+
+let listaNoticias = [
+{
+    titulo:"Amazon ahajajdbjabjdabjdbaj ",
+    descricao: "ajsbjgagjhasahvaa"
+},
+{
+    titulo:"dolar foda",
+descricao: ""},
+{
+    titulo:"",
+    descricao: ""
+},
+{
+    titulo:"",
+    descricao: ""
+},
+{
+    titulo:"",
+    descricao: ""
+},
+{
+    titulo:""
+    descricao: ""
+}
+
+]
